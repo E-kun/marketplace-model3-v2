d492e6fda1a00fa2bc525993eb75d1165d148b5f
E-kun <zentetsukenshi@gmail.com>
2023-06-18 10:54:16 +0800

    Cyclic Deployment build 18-06-2023 - Added home page

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 //Package definition references
 const express = require('express');
 const app = express();
-const dotenv = require("dotenv");
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const flash = require('connect-flash');
+
+const dotenv = require("dotenv");
 dotenv.config();
 
 //Local file definition references
 const { dbConnect, pgConnect, mongoClient, pgPool } = require('./utils/db-connect');
 const port = process.env.WEBAPPPORT;
+const ExpressError = require('./utils/ExpressError');
 
 //Schema model files
 const User = require('./models/user');
 const Resource = require('./models/resource');
 
+//Middleware
 app.set('view engine', 'ejs');
 app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
+// app.use(flash());
+
+// app.use((req, res, next) => {
+//     // console.log(req.session);
+//     res.locals.session = req.session;
+//     res.locals.currentUser = req.user;
+//     res.locals.success = req.flash('success');
+//     res.locals.error = req.flash('error');
+//     next();
+// });
 
 dbConnect().catch(console.dir);
 pgConnect();
 
-app.get('/', async (req, res, next) => {
+app.get('/', async (req, res) => {
     res.render('home');
 });
 
