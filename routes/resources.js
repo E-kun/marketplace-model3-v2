a6e5a87b0a6839f156ebda7b26f900f5b4f5fc70
E-kun <zentetsukenshi@gmail.com>
2023-07-10 18:19:40 +0800

    Cyclic Deployment 10/07/2023 - Changes made to Joi validation

--- routes/resources.js
+++ routes/resources.js
@@ -10,32 +10,21 @@ const upload = multer(storage);
 
 router.route('/')
     .get(catchAsync(resources.catalogue))
-    // .post(isLoggedIn, catchAsync(resources.createResouce));
     .post(upload.fields([
-        { name: 'image' },
-        { name: 'material', maxCount: 1 }
-        ]), catchAsync(resources.createResouce));
-    // .post(isLoggedIn, upload.fields([
-    //         { name: 'image' },
-    //         { name: 'material', maxCount: 1 }
-    //         ]), catchAsync(resources.createResouce));
-    // .post(upload.fields([
-    //         { name: 'image' },
-    //         { name: 'material', maxCount: 1 }
-    //         ]), catchAsync(resources.createResouce));
+            { name: 'image' },
+            { name: 'material', maxCount: 1 }
+            ]), catchAsync(resources.createResouce));
 
-    // validateResource,
-
-router.get('/new-resource', resources.renderNewForm);
+router.get('/new-resource', isLoggedIn, resources.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(resources.showResource))
     // .put(isLoggedIn, validateResource, catchAsync(resources.editResource))
-    .put(upload.none(), validateResource, catchAsync(resources.editResource))
+    .put(isLoggedIn, upload.none(), validateResource, catchAsync(resources.editResource))
     // .delete(isLoggedIn, catchAsync(resources.deleteResource));
-    .delete(catchAsync(resources.deleteResource));
+    .delete(isLoggedIn, catchAsync(resources.deleteResource));
 
 // router.get('/:id/edit', isLoggedIn, catchAsync(resources.renderEditForm));
-router.get('/:id/edit', catchAsync(resources.renderEditForm));
+router.get('/:id/edit', isLoggedIn, catchAsync(resources.renderEditForm));
 
 module.exports = router;
--- schemas.js
+++ schemas.js
@@ -2,11 +2,11 @@ const Joi = require('joi');
 
 module.exports.resourceSchema = Joi.object({
     resource: Joi.object({
-        name: Joi.string().alphanum().required(),
+        name: Joi.string().required().regex(/[$\(\)<>!;]/, { invert: true }),
         price: Joi.number().required().min(0),
         image: Joi.string().required(),
-        subject: Joi.string().alphanum().required(),
-        description: Joi.string().alphanum().required()
+        subject: Joi.string().required().regex(/[$\(\)<>!;]/, { invert: true }),
+        description: Joi.string().required().regex(/[$\(\)<>!]/, { invert: true })
     }).required()
 });
 
